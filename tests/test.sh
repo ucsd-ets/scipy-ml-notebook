fc92ee01be9864b4c5e3ae7b2e65d362b419a0c5
wesuuu <wuykimpang@ucsd.edu>
2020-09-30 10:43:39 -0700

    Updated tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-SCIPY_ML_TESTDIR=$TESTDIR/scipy-ml-notebook
-if ! jupyter nbconvert --execute "${SCIPY_ML_TESTDIR}/scipy-ml-notebook-gpu.ipynb"; then
+SCIPY_ML_TESTDIR=/usr/share/datahub/tests/scipy-ml-notebook
+
+if ! jupyter nbconvert --execute "${SCIPY_ML_TESTDIR}/scipy-ml-notebook-cpu.ipynb"; then
     echo "Integration test failed"
     echo "could not execute scipy-ml-notebook"
     exit 1
 fi
 
-if ! jupyter nbconvert --ExecutePreprocessor.kernel_name=ml-latest "${SCIPY_ML_TESTDIR}/scipy-ml-notebook-gpu.ipynb"; then
+if ! jupyter nbconvert --ExecutePreprocessor.kernel_name=ml-latest "${SCIPY_ML_TESTDIR}/scipy-ml-notebook-cpu.ipynb"; then
     echo "Integration test failed"
     echo "could not execute scipy-ml-notebook"
     exit 1
 fi
 
-if ! test -f "${SCIPY_ML_TESTDIR}/scipy-ml-notebook-gpu.html"; then
+if ! test -f "${SCIPY_ML_TESTDIR}/scipy-ml-notebook-cpu.html"; then
     echo "Integration test failed"
     echo "Compiled scipy-ml-notebook.html does not exist"
     exit 1
